8a5841cb834c2a66c9c61355fac45166fab5848a
Hibiki Tsuboi <hibiki.tsuboi@monstar-lab.com>
2021-05-16 22:35:54 +0900

    aa7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 function App() {
-  return <h1>Hello</h1>;
+  // const greeting = "Hi!";
+  // const dom = <h1>{greeting}</h1>;
+  // return dom;
+  return <input type="text" onClick={() => {console.log("I am clicked!")}} />;
 }
 
 export default App;
